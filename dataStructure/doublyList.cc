951f6848b52b024a397f305e07ece41d20a690ba
godelscat <ragneroc@163.com>
2018-07-29 01:55:55 +0200

    doubly linked list

---
+++ dataStructure/doublyList.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "mylist.h"
+using namespace std;
+
+int main ()
+{
+	doublyList<int> testList;
+	testList.ListInsert(1);
+	testList.ListInsert(4);
+	testList.ListInsert(16);
+	testList.ListInsert(9);
+	testList.ListShow();
+	node<int>* a = testList.ListSearch(16);
+	testList.ListDelete(a);
+	cout << "after delete" << endl;
+	testList.ListShow();
+	return 0;
+}
+
---
+++ dataStructure/mylist.h
@@ -0,0 +1,76 @@
+#ifndef MYLIST_H
+#define MYLIST_H
+#include <iostream>
+
+template <class T>
+struct node {
+	T x;
+	node* next;
+	node* prev;
+};
+
+template <class T>
+class doublyList {
+	private :
+		node<T>* head;
+
+	public :
+		doublyList();
+		~doublyList();
+		node<T>* ListSearch (T k);
+		void ListInsert (T  key);
+		void ListDelete (node<T>* key);
+		void ListShow ();
+};
+
+template <class T>
+doublyList<T>::doublyList() {
+	head = nullptr;
+}
+
+template <class T>
+doublyList<T>::~doublyList() {
+	node<T>* temp = nullptr;
+	while (head != nullptr) {
+		temp = head;
+		head = head->next;
+		delete temp;
+	}
+}
+
+template <class T>
+node<T>* doublyList<T>::ListSearch (T k) {
+	node<T>* temp = head;
+	while ( temp != nullptr && temp->x != k ) {
+		temp = temp->next;
+	}
+	return temp;
+}
+
+template <class T>
+void doublyList<T>::ListInsert (T  key) {
+	node<T>* temp = new node<T>;
+	temp->x = key;
+	temp->next = head;
+	if (head != nullptr) head->prev = temp;
+	head = temp;
+	temp->prev = nullptr;
+}
+
+template <class T>
+void doublyList<T>::ListDelete (node<T>* key) {
+	if (key->prev != nullptr) key->prev->next = key->next;
+	else head = key->next;
+	if (key->next != nullptr) key->next->prev = key->prev;
+}
+
+template <class T>
+void doublyList<T>::ListShow () {
+	node<T>* temp = head;
+	while (temp != nullptr) {
+		std::cout << temp->x << std::endl;
+		temp = temp->next;
+	}
+}
+
+#endif
