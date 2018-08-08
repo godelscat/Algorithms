fc37dbed3e470bb18d5ef3bb811a118770b287d3
godelscat <shimrac@outlook.com>
2018-08-09 00:17:50 +0200

    error when insert

--- data-structure/black-red-tree/BRTs.h
+++ data-structure/black-red-tree/BRTs.h
@@ -8,31 +8,35 @@ namespace BRTs
     struct node {
         int key;
         int val;
-        int color = 1; //default black
+        bool color = 1; //default black
         std::shared_ptr<node> parent;
         std::shared_ptr<node> left;
         std::shared_ptr<node> right;
+        node () { } ;
         node (int k, int v) : key(k), val(v) {};
     };
     using sptr = std::shared_ptr<node>  ;
 
     class tree {
         private :
-            sptr nil;
-            sptr root;
+            sptr nil ;
+            sptr root ;
             void inorder (sptr n);
             sptr min (sptr n);
             sptr search (int k);
             sptr ssor (sptr n);
+            void ins_fixup (sptr n);
+//            void del_fixup (sptr n);
         public :
             tree () {
+                nil = std::make_shared<node>();
                 root = nil;
             }
             void inshow () ;
-           // void left_rotate (sptr n);
-           // void right_rotate (sptr n);
+            void left_rotate (sptr n);
+            void right_rotate (sptr n);
             void ins (node &n);
-           // void del (node &n);
+ //           void del (node &n);
 
     };
 
@@ -74,6 +78,87 @@ namespace BRTs
         return temp;
     }
 
+    void tree::left_rotate (sptr n) {
+        //n is the root node of rotating subtree
+        sptr x = n->left; //right child to be rotated
+        n->right = x->left;
+        if (x->left != nil) x->left->parent = n;
+        x->parent = n->parent;
+        if ( n->parent == nil ) {
+            root = x;
+        } else {
+            if ( n = n->parent->left ) x = n->parent->left;
+            else x = n->parent->right;
+        }
+        n->parent = x;
+        x->left = n;
+    }
+
+    void tree::right_rotate(sptr n) {
+        //n is the root node of rotating subtree
+        sptr x = n->right; //left child to be rotated
+        n->left = x->right;
+        if (x->right != nil) x->right->parent = n;
+        x->parent = n->parent;
+        if ( n->parent == nil ) {
+            root = x;
+        } else {
+            if ( n = n->parent->left ) x = n->parent->left;
+            else x = n->parent->right;
+        }
+        n->parent = x;
+        x->right= n;
+    }
+
+    void tree::ins_fixup (sptr n) {
+        while ( ! n->parent->color ) {
+            if ( n->parent == n->parent->parent->left ) {
+                sptr uncle = n->parent->parent->right;
+                // case 1 : uncle is red
+                if ( ! uncle->color ) { 
+                    n->parent->color = 1;
+                    uncle->color = 1;
+                    n = n->parent->parent;
+                    n->parent->parent->color = 1;
+                    continue;
+                } //case 2 : uncle is black, and n is right child
+                //after rotate, case 2 becomes case 3
+                if ( uncle->color && n == n->parent->right ) {
+                    n = n->parent;
+                    tree::left_rotate (n);
+                }
+                //case 3 : uncle is black, and n is left child
+                if ( uncle->color && n == n->parent->left ) {
+                    n->parent->color = 1;
+                    n->parent->parent->color = 0;
+                    right_rotate(n->parent->parent);
+                }
+            } else {
+                sptr uncle = n->parent->parent->left;
+                // case 1 : uncle is red
+                if ( ! uncle->color ) { 
+                    n->parent->color = 1;
+                    uncle->color = 1;
+                    n->parent->parent->color = 1;
+                    n = n->parent->parent;
+                    continue;
+                } //case 2 : uncle is black, and n is left child
+                //after rotate, case 2 becomes case 3
+                if ( uncle->color && n == n->parent->left) {
+                    n = n->parent;
+                    tree::right_rotate(n);
+                }
+                //case 3 : uncle is black, and n is right child
+                if ( uncle->color && n == n->parent->right) {
+                    n->parent->color = 1;
+                    n->parent->parent->color = 0;
+                    left_rotate(n->parent->parent);
+                }
+            }
+        }
+        root->color = 1; //set root color to black
+    }
+
     void tree::ins (node &n) {
         sptr temp = std::make_shared<node> (n.key, n.val) ;
         sptr x = nil; //parent of insert node
@@ -83,7 +168,7 @@ namespace BRTs
             if ( y->key < n.key ) y = y->right;
             else y = y->left;
         }
-        temp->parent = x;
+        temp->parent = x; //set root->parent = nil
         if ( x == nil )  root = temp;
         else {
             if ( x->key < n.key ) x->right = temp;
@@ -91,6 +176,8 @@ namespace BRTs
         }
         temp->left = nil;
         temp->right = nil;
+        temp->color = 0; //set insert color to red
+        tree::ins_fixup (temp);
     }
 
 }
--- data-structure/black-red-tree/brt.cc
+++ data-structure/black-red-tree/brt.cc
@@ -4,11 +4,14 @@ using namespace std;
 
 int main ()
 {
-    BRTs::node n1(23,44), n2(11,24), n3(17,87), n4(53,12), n5(32,14), n6(76,25),
-            n7(18,9), n8(42,37), n9(9,10), n10(19,33);
+    BRTs::node n1(41,24), n2(38,14), n3(31,87), n4(12,27), n5(19,54), n6(8,25);
     BRTs::tree T;
-    T.ins (n1); T.ins(n2); T.ins(n3); T.ins(n4); T.ins(n5);
-    T.ins (n6); T.ins(n7); T.ins(n8); T.ins(n9); T.ins(n10);
+    T.ins (n1); 
+    T.ins(n2); 
+    T.ins(n3); 
+    T.ins(n4); 
+    T.ins(n5);
+    T.ins(n6);
     T.inshow();
    // T.del (n2); T.del(n6); T.del(n4);
    // cout << "After deletel some nodes" << endl;
